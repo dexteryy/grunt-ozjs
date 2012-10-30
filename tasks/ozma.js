c3296b96b635b98c2cdde593d9d72349bf696d99
dexteryy <dexter.yy@gmail.com>
2012-10-30 17:10:02 +0800

    0.1.5: remove lock

--- tasks/ozma.js
+++ tasks/ozma.js
@@ -4,7 +4,6 @@ module.exports = function(grunt){
     var path = require('path');
     var Ozma = require('ozma').Ozma;
     var DESC = 'enables you to use gruntfile to configure ozma and integrate with other grunt tasks';
-    var _lock;
 
     grunt.registerMultiTask('ozma', DESC, function() {
         var done = this.async();
@@ -12,12 +11,6 @@ module.exports = function(grunt){
     });
 
     grunt.registerHelper('ozma', function(opt, cb) {
-        if (_lock) {
-            grunt.log.write('The interval(' + (+new Date() - _lock) +
-                's) is shorter than ' + opt.debounceDelay + 's, skip...');
-            return cb();
-        }
-        _lock = +new Date();
         recurse_process(opt);
         opt = Object.create(opt);
         var src = opt.src;
@@ -34,12 +27,7 @@ module.exports = function(grunt){
                     .replace('{', '{\n  ').replace('}', '\n}'));
         }
         opt._ = [src];
-        Ozma()(opt, function(){
-            cb();
-            setTimeout(function(){
-                _lock = false;
-            }, opt.debounceDelay || 2000);
-        });
+        Ozma()(opt, cb);
     });
 
     function recurse_process(obj){
